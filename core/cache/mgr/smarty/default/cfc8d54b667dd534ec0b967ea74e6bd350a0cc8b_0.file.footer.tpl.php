bc72391db7ec4f31d5041cb9a2145637b2d50e5c
Unknown <cirus6161@gmail.com>
2018-05-21 15:32:07 +0300

    commit 1

---
+++ core/cache/mgr/smarty/default/cfc8d54b667dd534ec0b967ea74e6bd350a0cc8b_0.file.footer.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.31, created on 2018-05-21 15:22:08
+  from "C:\OSPanel\domains\LandingNerds.modx\smanager\templates\default\footer.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5b02b9f0244eb3_79266958',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'cfc8d54b667dd534ec0b967ea74e6bd350a0cc8b' => 
+    array (
+      0 => 'C:\\OSPanel\\domains\\LandingNerds.modx\\smanager\\templates\\default\\footer.tpl',
+      1 => 1526905285,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b02b9f0244eb3_79266958 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+    </div>
+    <!-- #modx-content-->
+    <div id="modx-footer"></div>
+</div>
+<!-- #modx-container -->
+
+</body>
+</html><?php }
+}
---
+++ core/cache/mgr/smarty/default/d30d844839a8bbdca8be05f875da2090a45b3243_0.file.welcome.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.31, created on 2018-05-21 15:22:08
+  from "C:\OSPanel\domains\LandingNerds.modx\smanager\templates\default\welcome.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_5b02b9f01c9361_68273276',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'd30d844839a8bbdca8be05f875da2090a45b3243' => 
+    array (
+      0 => 'C:\\OSPanel\\domains\\LandingNerds.modx\\smanager\\templates\\default\\welcome.tpl',
+      1 => 1526905285,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b02b9f01c9361_68273276 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div id="modx-panel-welcome-div"></div>
+
+<div id="modx-dashboard" class="dashboard">
+<?php echo $_smarty_tpl->tpl_vars['dashboard']->value;?>
+
+</div><?php }
+}
